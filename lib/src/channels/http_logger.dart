1c5af5c3e9c9d872a52a5b17583c7993486d653b
mahmoud saad <mahmoud1saad2@gmail.com>
2024-12-10 13:04:23 +0200

    convert to http

--- lib/src/channels/http_logger.dart
+++ lib/src/channels/http_logger.dart
@@ -1,7 +1,5 @@
 
 import 'dart:developer';
-import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart' as dio;
 import 'package:http_interceptor/http_interceptor.dart';
 
 import '../network_event.dart';
@@ -9,7 +7,7 @@ import '../network_logger.dart';
 
 class HttpNetworkLogger extends InterceptorContract {
   final NetworkEventList eventList;
-  final _requests = <dio.RequestOptions, NetworkEvent>{};
+  final _requests = <BaseRequest, NetworkEvent>{};
 
   HttpNetworkLogger({NetworkEventList? eventList})
       : this.eventList = eventList ?? NetworkLogger.instance;
@@ -21,16 +19,18 @@ class HttpNetworkLogger extends InterceptorContract {
     print('----- Request -----');
     print(request.toString());
     print(request.headers.toString());
-    eventList.add(_requests[convertHttpRequestToDioOptions(request)] = NetworkEvent.now(
-      request:  RequestCustom(
-        uri: request.url.toString(),
-        data: request.headers,
-        method: request.method,
-        headers: Headers(request.headers.entries.map(
-              (kv) => MapEntry(kv.key, '${kv.value}'),
-        )),
+    log('-----mano  Request -----event ${request.url.toString()}${request.hashCode}');
 
-    )));
+    eventList.add(_requests[request] = NetworkEvent.now(
+        request:  RequestCustom(
+          uri: request.url.toString(),
+          data: request.headers,
+          method: request.method,
+          headers: Headers(request.headers.entries.map(
+                (kv) => MapEntry(kv.key, '${kv.value}'),
+          )),
+
+        )));
     return request;
   }
 
@@ -43,37 +43,44 @@ class HttpNetworkLogger extends InterceptorContract {
     if (response is Response) {
       log((response).body);
     }
+    log('-----mano  Response -----event ${response.request?.url.toString()}${response.request.hashCode}');
 
-    final event = response.request!=null?_requests.remove(convertHttpRequestToDioOptions(response.request!)):null;
+    final event = response.request!=null?_requests.remove(response.request):null;
+    log('-----mano  Response -----event $event');
+
     if (event != null) {
-      eventList.updated(event..response =  ResponseCustom(
-        data:  (response is Response)?response.body:"",
-        statusCode: response.statusCode,
-        statusMessage: response.reasonPhrase ?? 'unkown',
-        headers: Headers(response.request?.headers?.entries?.map(
-              (kv) => MapEntry(kv.key, '${kv.value}'),
-        )??{},
-      )));
-    } else {
-    eventList.add(NetworkEvent.now(
-      request: RequestCustom(
-        uri: response.request?.url.toString()??"",
-        data: response.request?.headers,
-        method: response.request?.method??"",
-        headers: Headers(response.request?.headers?.entries?.map(
-              (kv) => MapEntry(kv.key, '${kv.value}'),
-        )??{}),
+      log('-----mano  update -----event $event');
 
-      ),
-      response: ResponseCustom(
-          data: (response is Response)?response.body:"",
-          statusCode: response.statusCode  ,
-          statusMessage: response.reasonPhrase ??  'unkown',
-          headers:Headers(response.request?.headers?.entries?.map(
+      eventList.updated(event..response =  ResponseCustom(
+          data:  (response is Response)?response.body:"",
+          statusCode: response.statusCode,
+          statusMessage: response.reasonPhrase ?? 'unkown',
+          headers: Headers(response.request?.headers?.entries?.map(
                 (kv) => MapEntry(kv.key, '${kv.value}'),
-          )??{},)
-      ),
-    ));
+          )??{},
+          )));
+    } else {
+      log('-----mano  add -----event $event');
+
+      eventList.add(NetworkEvent.now(
+        request: RequestCustom(
+          uri: response.request?.url.toString()??"",
+          data: response.request?.headers,
+          method: response.request?.method??"",
+          headers: Headers(response.request?.headers?.entries?.map(
+                (kv) => MapEntry(kv.key, '${kv.value}'),
+          )??{}),
+
+        ),
+        response: ResponseCustom(
+            data: (response is Response)?response.body:"",
+            statusCode: response.statusCode  ,
+            statusMessage: response.reasonPhrase ??  'unkown',
+            headers:Headers(response.request?.headers?.entries?.map(
+                  (kv) => MapEntry(kv.key, '${kv.value}'),
+            )??{},)
+        ),
+      ));
     }
 
     return response;
@@ -81,36 +88,36 @@ class HttpNetworkLogger extends InterceptorContract {
 
 
 
-  dio.RequestOptions convertHttpRequestToDioOptions(http.BaseRequest request) {
-    // Parse query parameters from the URL
-    final uri = request.url;
-    final queryParameters = Map<String, dynamic>.from(uri.queryParameters);
-
-    // Extract headers
-    final headers = Map<String, String>.from(request.headers);
-
-    // Extract body (if available, typically for POST/PUT requests)
-    var data;
-    if (request is http.Request && request.body.isNotEmpty) {
-      // If the request body is JSON or any other string
-      data = request.body;
-    } else if (request is http.MultipartRequest) {
-      // Handle multipart request (for file uploads)
-      data = dio.FormData.fromMap({
-        ...request.fields,
-      });
-    }
-
-    // Create and return Dio RequestOptions
-    return dio.RequestOptions(
-      path: uri.path,
-      method: request.method,
-      baseUrl: '${uri.scheme}://${uri.host}',
-      queryParameters: queryParameters,
-      headers: headers,
-      data: data,
-    );
-  }
+// dio.RequestOptions convertHttpRequestToDioOptions(http.BaseRequest request) {
+//   // Parse query parameters from the URL
+//   final uri = request.url;
+//   final queryParameters = Map<String, dynamic>.from(uri.queryParameters);
+//
+//   // Extract headers
+//   final headers = Map<String, String>.from(request.headers);
+//
+//   // Extract body (if available, typically for POST/PUT requests)
+//   var data;
+//   if (request is http.Request && request.body.isNotEmpty) {
+//     // If the request body is JSON or any other string
+//     data = request.body;
+//   } else if (request is http.MultipartRequest) {
+//     // Handle multipart request (for file uploads)
+//     data = dio.FormData.fromMap({
+//       ...request.fields,
+//     });
+//   }
+//
+//   // Create and return Dio RequestOptions
+//   return dio.RequestOptions(
+//     path: uri.path,
+//     method: request.method,
+//     baseUrl: '${uri.scheme}://${uri.host}',
+//     queryParameters: queryParameters,
+//     headers: headers,
+//     data: data,
+//   );
+// }
 
 }
 
